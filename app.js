e512f1a43e151b3160cd88db2f4d6799cd1637e6
fatih <fthhbsr@gmail.com>
2022-06-08 12:11:34 +0300

    fix

--- app.js
+++ app.js
@@ -23,22 +23,24 @@ let list = document.getElementById("list");
 
 add.addEventListener("click", () => {
   console.log(input.value);
+  let trim = input.value.trim();
+  if (trim) {
+    let row = document.createElement("div");
+    list.appendChild(row);
 
-  let row = document.createElement("div");
-  list.appendChild(row);
-
-  let frame = `<li id="${idCount}" class="list-item">
+    let frame = `<li id="${idCount}" class="list-item">
             <input type="checkbox" name="check" class="list-item"/>
             <span class="to-do">${input.value}</span>
             <i class="fa-solid fa-trash-can"></i>
           </li>`;
-  arr.push([idCount, true]);
-  console.log(arr);
-  list.lastElementChild.innerHTML = frame;
-  console.log();
-  idCount++;
-  totalCount++;
-  events();
+    arr.push([idCount, true]);
+    console.log(arr);
+    list.lastElementChild.innerHTML = frame;
+    console.log();
+    idCount++;
+    totalCount++;
+    events();
+  }
   input.value = "";
 });
 
