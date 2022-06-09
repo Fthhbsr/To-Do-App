86c9f861580ca2a411fd6938c7baf81317b11f97
fatih <fthhbsr@gmail.com>
2022-06-09 03:08:09 +0300

    localstorage added

--- app.js
+++ app.js
@@ -1,79 +1,65 @@
 let input = document.getElementById("input");
-// console.log(input);
 
-let add = document.getElementById("add");
-// console.log(add);
 let total = document.querySelector("#total");
-// console.log(total);
+
 let completed = document.querySelector("#completed");
-// console.log(completed);
-let checkCount = 0;
-let totalCount = 0;
-let idCount = 0;
-let arr = [];
+
+let container = document.querySelector(".container");
+
 let list = document.getElementById("list");
 
-// console.log(todo);
+let todos = [];
 
-// let frame = `<li class="list-item">
-//             <input type="checkbox" name="check" class="list-item"/>
-//             <span class="to-do"></span>
-//             <i class="fa-solid fa-trash-can"></i>
-//           </li>`;
+//load prosess
+window.addEventListener("load", () => {
+  let readItem = JSON.parse(localStorage.getItem("todos"));
+  list.innerHTML += readItem[0];
+  events();
+  input.focus();
+});
 
-add.addEventListener("click", () => {
-  console.log(input.value);
-  let trim = input.value.trim();
-  if (trim) {
-    let row = document.createElement("div");
-    list.appendChild(row);
-
-    let frame = `<li id="${idCount}" class="list-item">
-            <input type="checkbox" name="check" class="list-item"/>
+container.addEventListener("click", (e) => {
+  // add item
+  if (e.target.classList.contains("add")) {
+    let trim = input.value.trim();
+    if (trim) {
+      let frame = `<li class="list-item">
+            <input type="checkbox" name="check" class="checkbox"  />
             <span class="to-do">${input.value}</span>
             <i class="fa-solid fa-trash-can"></i>
           </li>`;
-    arr.push([idCount, true]);
-    console.log(arr);
-    list.lastElementChild.innerHTML = frame;
-    console.log();
-    idCount++;
-    totalCount++;
-    events();
+
+      list.innerHTML += frame;
+    }
+    input.value = "";
   }
-  input.value = "";
-});
 
-let toDoSection = document.querySelector(".to-do-section");
-
-toDoSection.addEventListener("click", (e) => {
-  let checkStatus = arr[e.target.parentElement.id][1];
+  //remove item
   if (e.target.classList.contains("fa-solid")) {
-    if (!checkStatus) {
-      checkCount--;
-    }
-    e.target.parentElement.parentElement.remove();
-    totalCount--;
-    events();
+    e.target.parentElement.remove();
   }
-  if (e.target.classList.contains("list-item")) {
-    if (checkStatus) {
-      arr[e.target.parentElement.id][1] = false;
-      checkCount++;
-    } else {
-      arr[e.target.parentElement.id][1] = true;
-      checkCount--;
-    }
-    events();
-    if (checkStatus) {
+
+  //checked item
+  if (e.target.classList.contains("checkbox")) {
+    if (e.target.checked) {
       e.target.nextElementSibling.style = "text-decoration:line-through";
+      e.target.setAttribute("checked", true);
     } else {
       e.target.nextElementSibling.style = "text-decoration:none";
+      e.target.removeAttribute("checked");
     }
   }
+
+  events();
+  //localstorage
+  let arr = [list.innerHTML];
+  localStorage.setItem("todos", JSON.stringify(arr));
 });
 
+//total and completed items
 let events = function () {
-  total.innerHTML = totalCount;
-  completed.innerHTML = checkCount;
+  total.innerHTML = Array.from(document.querySelectorAll(".list-item")).length;
+  completed.innerHTML = Array.from(
+    document.querySelectorAll("input:checked")
+  ).length;
 };
